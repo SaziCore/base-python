d02b4b519c9240675e37fc7e831b5a082a8bc62b
SaziCore <markbikese@gmail.com>
2026-03-28 21:11:54 +0300

    done

---
+++ day_1/main.py
@@ -0,0 +1,23 @@
+hello="Hello, World!"
+print(hello)
+
+user_name = "SaziCore"
+current_day = 1
+coding_hours = 2.0
+is_beginner = True
+
+print("--- ОТЧЕТ ПОЛЬЗОВАТЕЛЯ: ", user_name, " ---")
+print("Сегодня мой день номер: ", current_day)
+print("Я занимался программированием уже ", coding_hours, " часа(ов).")
+
+
+target_days = 1000
+days_left = target_days - current_day
+
+print("------------------------------")
+print("МОЙ ГРАНДИОЗНЫЙ ПЛАН:")
+print("1. Изучать Python каждый день без пропусков.")
+print("2. Сохранять каждый шаг в GitHub репозиторий.")
+print("3. Идти к цели в ", target_days, " дней.")
+print("Осталось пройти шагов: ", days_left)
+print("------------------------------")
